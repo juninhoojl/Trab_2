90ad74441ab5dd4d9a4fe78d01a167e92a95cef0
juninhoojl <juninhopc@icloud.com>
2019-04-04 10:48:59 -0300

    Header file

---
+++ Trab_2/lancamento.h
@@ -0,0 +1,31 @@
+//
+//  lancamento.h
+//  Trab_2
+//
+//  Created by José Luiz Junior on 04/04/19.
+//  Copyright © 2019 José Luiz Junior. All rights reserved.
+//
+
+#ifndef lancamento_h
+#define lancamento_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+//Para considerar o angulo -45 e + 45
+//Recalcular a distancia minima que precisa ser atingida
+
+
+//Retorna distancia maxima
+
+
+//Retorna
+
+float distancia(float v0, float v, float a);
+float dmax(float v0, float o);
+float vy(float v, float o);
+float vx(float v, float o);
+float dalvo(float o);
+
+#endif /* lancamento_h */

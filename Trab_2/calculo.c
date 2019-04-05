e181147647b66823fd6a7c44ebeed4990eb41484
juninhoojl <juninhopc@icloud.com>
2019-04-05 11:02:08 -0300

    Versão nova

---
+++ Trab_2/calculo.c
@@ -0,0 +1,9 @@
+//
+//  calculo.c
+//  Trab_2
+//
+//  Created by José Luiz Junior on 05/04/19.
+//  Copyright © 2019 José Luiz Junior. All rights reserved.
+//
+
+#include "calculo.h"
---
+++ Trab_2/calculo.h
@@ -0,0 +1,14 @@
+//
+//  calculo.h
+//  Trab_2
+//
+//  Created by José Luiz Junior on 05/04/19.
+//  Copyright © 2019 José Luiz Junior. All rights reserved.
+//
+
+#ifndef calculo_h
+#define calculo_h
+
+#include <stdio.h>
+
+#endif /* calculo_h */
